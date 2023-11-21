1d71e2f0012399a0a02c3cc756931446cb6958e1
Brent Ross Habawel <rosshabawel12@gmail.com>
2023-11-21 14:22:35 +0800

    builded the staitc form in paying the bill

--- src/App.js
+++ src/App.js
@@ -25,7 +25,9 @@ export default function App() {
       <div className="sidebar">
         <FriendsList />
         <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -76,6 +78,31 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill Value</label>
+      <input type="text" />
+
+      <label>🕴️ Your expenses</label>
+      <input type="text" />
+
+      <label>👯 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
     </form>
   );
 }
